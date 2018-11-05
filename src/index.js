60b26c6f72e15de3662623d419155f41c11f3e70
Ivan <aiven715@gmail.com>
2018-11-05 10:27:30 +0300

    Use regular function instead of async

--- src/index.js
+++ src/index.js
@@ -9,29 +9,30 @@ export const usePromise = (
   const [lastUpdated, setLastUpdated] = useState();
   const [error, setError] = useState();
 
-  const request = async (...args) => {
+  const request = (...args) => {
     /*
     Using isValid guard, in order to prevent the cleanup warning.
     */
     let isValid = true;
     setLoading(true);
 
-    try {
-      if (!isValid) return;
+    fn(...args)
+      .then(result => {
+        if (!isValid) return;
 
-      const result = await fn(...args);
+        setData(result);
+        setLastUpdated(Date.now());
+      })
+      .catch(err => {
+        if (!isValid) return;
 
-      setData(result);
-      setLastUpdated(Date.now());
-    } catch (err) {
-      if (!isValid) return;
+        setError(err);
+      })
+      .finally(() => {
+        if (!isValid) return;
 
-      setError(err);
-    } finally {
-      if (!isValid) return;
-
-      setLoading(false);
-    }
+        setLoading(false);
+      });
 
     /*
     When component will be unmounted, isValid will become false and state setter
