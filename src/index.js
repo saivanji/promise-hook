fa351edf6a97e97324de58acdbcb195864111354
Ivan <aiven715@gmail.com>
2018-11-05 10:20:50 +0300

    Add guard logic to prevent React cleanup warning. Fixes #1

--- src/index.js
+++ src/index.js
@@ -10,18 +10,36 @@ export const usePromise = (
   const [error, setError] = useState();
 
   const request = async (...args) => {
+    /*
+    Using isValid guard, in order to prevent the cleanup warning.
+    */
+    let isValid = true;
     setLoading(true);
 
     try {
+      if (!isValid) return;
+
       const result = await fn(...args);
 
       setData(result);
       setLastUpdated(Date.now());
     } catch (err) {
+      if (!isValid) return;
+
       setError(err);
     } finally {
+      if (!isValid) return;
+
       setLoading(false);
     }
+
+    /*
+    When component will be unmounted, isValid will become false and state setter
+    functions will not be envoked on unmounted component.
+    */
+    return () => {
+      isValid = false;
+    };
   };
 
   if (resolve) {
